a9f8f0856efa21e1701b3fbbfd3e4200e8625fe7
Alexander S <alx.sherwin@gmail.com>
2020-11-29 23:40:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
+  reversed_array = new_array.collect {|x| x.reverse!}
   reversed_array.join(" ")
 end
   
